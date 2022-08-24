e05e2b8d5e9d59df9ca3f9ad444480ee5fa890d5
OksanaKutsa <kutcaoksana@gmail.com>
2022-08-24 16:47:28 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <footer>
-        <a
-          href="https://github.com/OksanaKutsa/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-sourse code{" "}
-        </a>{" "}
-        made by
-        <a href="http://oksanakutsa.com/" target="_blank" rel="noreferrer">
-          {" "}
-          Oksana Kutsa{" "}
-        </a>
-      </footer>
+      <div className="continer">
+        Weather app
+        <footer>
+          <a
+            href="https://github.com/OksanaKutsa/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourse code{" "}
+          </a>{" "}
+          made by
+          <a href="http://oksanakutsa.com/" target="_blank" rel="noreferrer">
+            {" "}
+            Oksana Kutsa{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
