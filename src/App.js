1cc50d32433c3ab1b79fd4bbb79b0dc25eb68f67
OksanaKutsa <kutcaoksana@gmail.com>
2022-08-24 15:30:47 +0100

    added link to github and personal site

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      Weather app
+      <footer>
+        <a
+          href="https://github.com/OksanaKutsa/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-sourse code{" "}
+        </a>{" "}
+        made by
+        <a href="http://oksanakutsa.com/" target="_blank" rel="noreferrer">
+          {" "}
+          Oksana Kutsa{" "}
+        </a>
+      </footer>
+    </div>
+  );
 }
