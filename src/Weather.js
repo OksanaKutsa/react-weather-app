6145dab8cd7e700f21884a2b8c23589bb3f47609
OksanaKutsa <kutcaoksana@gmail.com>
2022-08-26 13:19:22 +0100

    working on weather css

--- src/Weather.js
+++ src/Weather.js
@@ -10,10 +10,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -22,10 +27,14 @@ export default function Weather() {
         <li>Wednesday 16:52</li>
         <li>Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img src="" alt="cloud" />
-          15˚C
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt="cloud"
+          />
+          <span className="temperature">15</span>{" "}
+          <span className="unit">˚C</span>
         </div>
         <div className="col-6">
           <ul>
